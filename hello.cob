e1b732d648ed0e811bb8d0ebd7dcfac6485c8ebe
Nichtaurelius <aurelius.pilat@hsbi.de>
2025-05-06 09:40:48 +0200

    Neue Textausgabeänderung

--- hello.cob
+++ hello.cob
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
        PROCEDURE DIVISION.
-       DISPLAY "Hello, COBOL! Hello Aurelius!".
+       DISPLAY "Hello, COBOL! Hello Aurelius! Das ist ein Git Test :)".
        STOP RUN.
