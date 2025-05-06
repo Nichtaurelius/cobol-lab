d9699903790b4ea244a877c9b9d3eb03239e2b89
Nichtaurelius <aurelius.pilat@hsbi.de>
2025-05-06 10:43:06 +0200

    Added performance calculation and output.

--- dwzRechner.cob
+++ dwzRechner.cob
@@ -5,17 +5,37 @@
        WORKING-STORAGE SECTION.
        01  BENUTZERZAHL     PIC 9(4).
        01  GEGNERZAHL       PIC 9(4).
-       01  ERGEBNIS         PIC A(1).
+       01  ERGEBNIS         PIC 9V9 VALUE 0.0.
+       01  PERFORMANCE    PIC 9(5).
        
        PROCEDURE DIVISION.
        DISPLAY "Bitte gib deine DWZ ein:"
        ACCEPT BENUTZERZAHL
-       DISPLAY "Bitte gib die DWZ deinses Gegners ein:"
+       DISPLAY "Bitte gib die DWZ deines Gegners ein:"
        ACCEPT GEGNERZAHL
-       DISPLAY "Bitte gib das Ergebnis der Partie ein (l/d/w):"
+       DISPLAY "Bitte gib das Ergebnis der Partie ein ",
+       "(1.0 = Sieg, 0.5 = Remis, 0.0 = Niederlage):"
        ACCEPT ERGEBNIS
-       IF ERGEBNIS = "w"
+       
+       IF ERGEBNIS = "1.0"
        DISPLAY "Gut gemacht. Du hast gegen nen ", GEGNERZAHL,
-       "gewonnen! :)"
+       " gewonnen! :)"
+       COMMPUTE PERFORMANCE = GEGNERZAHL + 400
+       DISPLAY "Deine Performance liegt bei ", PERFORMANCE
        END-IF
+
+       IF ERGEBNIS = "0.5"
+       DISPLAY "Ein Unentschieden gegen einen ", GEGNERZAHL,
+       " ist auch ganz okay. Immerhin hast du ja nicht verloren ;)"
+       COMMPUTE PERFORMANCE = GEGNERZAHL
+       DISPLAY "Deine Performance liegt bei ", PERFORMANCE
+       END-IF
+       
+       IF ERGEBNIS = "0"
+       DISPLAY "Schade, dass du gegen einen ", GEGNERZAHL,
+       " verloren hast. Das nächste mal wird es aber besser ;)"
+       COMMPUTE PERFORMANCE = GEGNERZAHL - 400
+       DISPLAY "Deine Performance liegt bei ", PERFORMANCE
+       END-IF
+
        STOP RUN.
