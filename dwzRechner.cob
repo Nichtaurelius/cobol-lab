72cfb80610a5f7f08a214ca0f6f6fb4a31a0a790
Nichtaurelius <aurelius.pilat@hsbi.de>
2025-05-06 10:48:18 +0200

    Changed result input type

--- dwzRechner.cob
+++ dwzRechner.cob
@@ -5,8 +5,8 @@
        WORKING-STORAGE SECTION.
        01  BENUTZERZAHL     PIC 9(4).
        01  GEGNERZAHL       PIC 9(4).
-       01  ERGEBNIS         PIC 9V9 VALUE 0.0.
-       01  PERFORMANCE    PIC 9(5).
+       01  ERGEBNIS         PIC A(1).
+       01  PERFORMANCE      PIC 9(5).
        
        PROCEDURE DIVISION.
        DISPLAY "Bitte gib deine DWZ ein:"
@@ -14,24 +14,24 @@
        DISPLAY "Bitte gib die DWZ deines Gegners ein:"
        ACCEPT GEGNERZAHL
        DISPLAY "Bitte gib das Ergebnis der Partie ein ",
-       "(1.0 = Sieg, 0.5 = Remis, 0.0 = Niederlage):"
+       "(w = Sieg, d = Remis, l = Niederlage):"
        ACCEPT ERGEBNIS
        
-       IF ERGEBNIS = "1.0"
+       IF ERGEBNIS = "w"
        DISPLAY "Gut gemacht. Du hast gegen nen ", GEGNERZAHL,
        " gewonnen! :)"
        COMPUTE PERFORMANCE = GEGNERZAHL + 400
        DISPLAY "Deine Performance liegt bei ", PERFORMANCE
        END-IF
 
-       IF ERGEBNIS = "0.5"
+       IF ERGEBNIS = "d"
        DISPLAY "Ein Unentschieden gegen einen ", GEGNERZAHL,
        " ist auch ganz okay. Immerhin hast du ja nicht verloren ;)"
        COMPUTE PERFORMANCE = GEGNERZAHL
        DISPLAY "Deine Performance liegt bei ", PERFORMANCE
        END-IF
        
-       IF ERGEBNIS = "0"
+       IF ERGEBNIS = "l"
        DISPLAY "Schade, dass du gegen einen ", GEGNERZAHL,
        " verloren hast. Das nächste mal wird es aber besser ;)"
        COMPUTE PERFORMANCE = GEGNERZAHL - 400
