4ac7e90f38e3fc79208b370a643cdbd2d30a0f97
Nichtaurelius <aurelius.pilat@hsbi.de>
2025-05-07 10:29:27 +0200

    If conditions fixed, Capital input now included

--- src/dwz.cob
+++ src/dwz.cob
@@ -9,28 +9,30 @@
        01  PERFORMANCE      PIC 9(4).
        
        PROCEDURE DIVISION.
-       PERFORM UNTIL BENUTZERZAHL > 400 OR BENUTZERZAHL < 3000
+       PERFORM UNTIL BENUTZERZAHL > 400 AND BENUTZERZAHL < 3000
        DISPLAY "Bitte gib deine DWZ ein:"
        ACCEPT BENUTZERZAHL
-       IF BENUTZERZAHL > 400 OR BENUTZERZAHL < 3000
+       IF BENUTZERZAHL < 400 OR BENUTZERZAHL > 3000
        DISPLAY "Bitte gib eine gültige DWZ ein (400 - 3000):"
        END-IF
        END-PERFORM
 
-       PERFORM UNTIL GEGNERZAHL > 400 OR GEGNERZAHL < 3000
+       PERFORM UNTIL GEGNERZAHL > 400 AND GEGNERZAHL < 3000
        DISPLAY "Bitte gib die DWZ deines Gegners ein:"
        ACCEPT GEGNERZAHL
-       IF GEGNERZAHL > 400 OR GEGNERZAHL < 3000
+       IF GEGNERZAHL < 400 OR GEGNERZAHL > 3000
        DISPLAY "Bitte gib eine gültige DWZ ein (400 - 3000):"
        END-IF
        END-PERFORM
 
        PERFORM UNTIL ERGEBNIS = "w" OR ERGEBNIS = "d" OR ERGEBNIS = "l"
+       OR ERGEBNIS = "W" OR ERGEBNIS = "D" OR ERGEBNIS = "L"
        DISPLAY "Bitte gib das Ergebnis der Partie ein ",
        "(w = Sieg, d = Remis, l = Niederlage):"
        ACCEPT ERGEBNIS
        IF ERGEBNIS NOT = "w" AND ERGEBNIS NOT = "d" AND
-       ERGEBNIS NOT = "l"
+       ERGEBNIS NOT = "l" AND ERGEBNIS NOT = "W" AND ERGEBNIS NOT = "D" 
+       AND ERGEBNIS NOT = "L" 
        DISPLAY "ungültige Eingabe!"
        END-IF
        END-PERFORM
